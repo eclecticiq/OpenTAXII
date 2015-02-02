86e7f7ad7afb53fa4d5b000508397ff9a72ae2c7
Sergey Polzunov <sergey@polzunov.com>
2015-02-02 13:50:31 +0100

    runner simplified

--- runner.py
+++ runner.py
@@ -1,12 +1,6 @@
-import logging
-logging.getLogger().setLevel(logging.DEBUG)
-
 from taxii_server.http import app
 
-def run_server():
-    app.debug = True
+if __name__ == '__main__':
+
     app.run(port=9000)
 
-
-if __name__ == '__main__':
-    run_server()
