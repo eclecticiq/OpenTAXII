11df3a24729c02d90a0b40ee153076572e8354f8
Sergey Polzunov <sergey@polzunov.com>
2015-02-02 14:15:19 +0100

    removing unnecessary code. depending on intelworks-common for now

--- taxii_server/utils.py
+++ taxii_server/utils.py
@@ -28,33 +28,5 @@ class SimpleRenderer(object):
 
 
 
-def configure_logging(level, plain_log=False):
-
-    if not plain_log:
-        intelworks.logging.configure(level)
-    else:
-        #renderer = structlog.processors.JSONRenderer() if not plain_log else SimpleRenderer()
-        renderer = SimpleRenderer()
-
-        structlog.configure(
-            processors = [
-                structlog.stdlib.filter_by_level,
-                structlog.stdlib.add_logger_name,
-                structlog.stdlib.add_log_level,
-                structlog.stdlib.PositionalArgumentsFormatter(),
-                structlog.processors.TimeStamper(fmt='iso'),
-                structlog.processors.StackInfoRenderer(),
-                structlog.processors.format_exc_info,
-                renderer
-            ],
-            context_class = dict,
-            logger_factory = structlog.stdlib.LoggerFactory(),
-            wrapper_class = structlog.stdlib.BoundLogger,
-            cache_logger_on_first_use = True,
-        )
-
-        handler = logging.StreamHandler(sys.stdout)
-        root_logger = logging.getLogger()
-        root_logger.addHandler(handler)
-
-    logging.getLogger().setLevel(level.upper())
+def configure_logging(level):
+    intelworks.logging.configure(level)
