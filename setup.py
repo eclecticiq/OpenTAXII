57a823e0c3f100f74bf09b13faa16b28bb2c2b7b
Sergey Polzunov <sergey@polzunov.com>
2015-03-16 18:08:40 +0100

    new dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "opentaxii",
-    version = "0.0.1",
+    name = 'opentaxii',
+    version = '0.0.2',
 
     packages = find_packages(),
     include_package_data = True,
@@ -11,17 +11,19 @@ setup(
         'opentaxii' : ['*.yml']
     },
 
+    dependency_links = [
+        'git+https://github.com/TAXIIProject/libtaxii.git#egg=libtaxii-1.1.106'
+    ],
+
     install_requires = [
-        'libtaxii==1.1.105-SNAPSHOT',
         'pytz==2014.10',
-        'intelworks-common',
         'pyyaml',
         'anyconfig',
         'structlog',
         'Flask',
         'sqlalchemy',
         'blinker',
-        'gunicorn',
-        'redis'
+        'bcrypt',
+        'pyjwt'
     ],
 )
