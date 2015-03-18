470ce231eb99eefc0e23f94364e6765afc6a17b8
Sergey Polzunov <sergey@polzunov.com>
2015-03-18 17:19:43 +0100

    dev runner should use logging levels from config file

--- opentaxii/cli/run.py
+++ opentaxii/cli/run.py
@@ -2,12 +2,15 @@
 from opentaxii.server import create_server
 from opentaxii.middleware import create_app
 from opentaxii.utils import configure_logging
+from opentaxii.config import ServerConfig
 
-configure_logging({'' : 'debug'}, plain=True)
 
 def run_in_dev_mode():
 
-    server = create_server()
+    config = ServerConfig()
+    configure_logging(config['logging'], plain=True)
+
+    server = create_server(config)
 
     app = create_app(server)
     app.debug = True
