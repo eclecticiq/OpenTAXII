f72057a9cb682155012e8f974d14ddcb4e8014f2
Sergey Polzunov <sergey@polzunov.com>
2015-03-19 16:36:39 +0100

    fixing example script

--- examples/create-fixtures.sh
+++ examples/create-fixtures.sh
@@ -1,7 +1,7 @@
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-export TAXII_SERVER_CONFIG=$DIR/config.yml
+export OPENTAXII_CONFIG=$DIR/config.yml
 
 opentaxii-create-services -c $DIR/services.yml
 opentaxii-create-collections -c $DIR/collections.yml
