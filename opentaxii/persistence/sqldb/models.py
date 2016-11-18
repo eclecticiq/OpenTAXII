4946d3fde63c7f128b7ebf58e862febd8782fecd
Sergey Polzunov <sergey@polzunov.com>
2016-11-18 12:10:37 +0100

    use strings of specific length + style tweaks

--- opentaxii/persistence/sqldb/models.py
+++ opentaxii/persistence/sqldb/models.py
@@ -1,11 +1,8 @@
 import json
 from datetime import datetime
 
+from sqlalchemy import schema, types
 from sqlalchemy.orm import relationship, validates
-from sqlalchemy.schema import (
-    Table, Column, ForeignKey, PrimaryKeyConstraint
-)
-from sqlalchemy.types import Integer, String, DateTime, Boolean, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 __all__ = ['Base', 'ContentBlock', 'DataCollection', 'Service',
@@ -17,19 +14,20 @@ Base = declarative_base(name='Model')
 class AbstractModel(Base):
     __abstract__ = True
 
-    date_created = Column(DateTime(timezone=True), default=datetime.utcnow)
+    date_created = schema.Column(
+        types.DateTime(timezone=True), default=datetime.utcnow)
 
 
-collection_to_content_block = Table(
+collection_to_content_block = schema.Table(
     'collection_to_content_block',
     Base.metadata,
-    Column(
-        'collection_id', Integer,
-        ForeignKey('data_collections.id')),
-    Column(
-        'content_block_id', Integer,
-        ForeignKey('content_blocks.id'), index=True),
-    PrimaryKeyConstraint('collection_id', 'content_block_id')
+    schema.Column(
+        'collection_id', types.Integer,
+        schema.ForeignKey('data_collections.id')),
+    schema.Column(
+        'content_block_id', types.Integer,
+        schema.ForeignKey('content_blocks.id'), index=True),
+    schema.PrimaryKeyConstraint('collection_id', 'content_block_id')
 )
 
 
@@ -37,22 +35,23 @@ class ContentBlock(AbstractModel):
 
     __tablename__ = 'content_blocks'
 
-    id = Column(Integer, primary_key=True)
-    message = Column(Text, nullable=True)
+    id = schema.Column(types.Integer, primary_key=True)
+    message = schema.Column(types.Text, nullable=True)
 
-    timestamp_label = Column(
-        DateTime(timezone=True), default=datetime.utcnow, index=True)
+    timestamp_label = schema.Column(
+        types.DateTime(timezone=True),
+        default=datetime.utcnow, index=True)
 
-    inbox_message_id = Column(
-        Integer,
-        ForeignKey(
+    inbox_message_id = schema.Column(
+        types.Integer,
+        schema.ForeignKey(
             'inbox_messages.id', onupdate='CASCADE', ondelete='CASCADE'),
         nullable=True)
 
-    content = Column(Text)
+    content = schema.Column(types.Text)
 
-    binding_id = Column(Text, index=True)
-    binding_subtype = Column(Text, index=True)
+    binding_id = schema.Column(types.Text, index=True)
+    binding_subtype = schema.Column(types.Text, index=True)
 
     collections = relationship(
         'DataCollection',
@@ -74,12 +73,14 @@ class ContentBlock(AbstractModel):
                 'binding={obj.binding_subtype})').format(obj=self)
 
 
-service_to_collection = Table(
+service_to_collection = schema.Table(
     'service_to_collection',
     Base.metadata,
-    Column('service_id', String, ForeignKey('services.id')),
-    Column('collection_id', Integer, ForeignKey('data_collections.id')),
-    PrimaryKeyConstraint('service_id', 'collection_id')
+    schema.Column('service_id',
+                  types.String(150), schema.ForeignKey('services.id')),
+    schema.Column('collection_id',
+                  types.Integer, schema.ForeignKey('data_collections.id')),
+    schema.PrimaryKeyConstraint('service_id', 'collection_id')
 )
 
 
@@ -87,17 +88,18 @@ class Service(AbstractModel):
 
     __tablename__ = 'services'
 
-    id = Column(String, primary_key=True)
-    type = Column(String)
+    id = schema.Column(types.String(150), primary_key=True)
+    type = schema.Column(types.String(150))
 
-    _properties = Column(Text, nullable=False)
+    _properties = schema.Column(types.Text, nullable=False)
 
     collections = relationship(
         'DataCollection',
         secondary=service_to_collection,
         backref='services')
 
-    date_updated = Column(DateTime(timezone=True), default=datetime.utcnow)
+    date_updated = schema.Column(
+        types.DateTime(timezone=True), default=datetime.utcnow)
 
     @property
     def properties(self):
@@ -112,17 +114,17 @@ class DataCollection(AbstractModel):
 
     __tablename__ = 'data_collections'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, index=True, unique=True)
+    id = schema.Column(types.Integer, primary_key=True)
+    name = schema.Column(types.Text, index=True, unique=True)
 
-    type = Column(String)
-    description = Column(Text, nullable=True)
+    type = schema.Column(types.String(150))
+    description = schema.Column(types.Text, nullable=True)
 
-    accept_all_content = Column(Boolean, default=False)
-    bindings = Column(Text)
+    accept_all_content = schema.Column(types.Boolean, default=False)
+    bindings = schema.Column(types.Text)
 
-    available = Column(Boolean, default=True)
-    volume = Column(Integer, default=0)
+    available = schema.Column(types.Boolean, default=True)
+    volume = schema.Column(types.Integer, default=0)
 
     def __repr__(self):
         return ('DataCollection(name={obj.name}, type={obj.type})'
@@ -133,31 +135,32 @@ class InboxMessage(AbstractModel):
 
     __tablename__ = 'inbox_messages'
 
-    id = Column(Integer, primary_key=True)
+    id = schema.Column(types.Integer, primary_key=True)
 
-    message_id = Column(Text)
-    result_id = Column(Text, nullable=True)
+    message_id = schema.Column(types.Text)
+    result_id = schema.Column(types.Text, nullable=True)
 
-    record_count = Column(Integer, nullable=True)
-    partial_count = Column(Boolean, default=False)
+    record_count = schema.Column(types.Integer, nullable=True)
+    partial_count = schema.Column(types.Boolean, default=False)
 
-    subscription_collection_name = Column(Text, nullable=True)
-    subscription_id = Column(Text, nullable=True)
+    subscription_collection_name = schema.Column(types.Text, nullable=True)
+    subscription_id = schema.Column(types.Text, nullable=True)
 
-    exclusive_begin_timestamp_label = Column(
-        DateTime(timezone=True), nullable=True)
-    inclusive_end_timestamp_label = Column(
-        DateTime(timezone=True), nullable=True)
+    exclusive_begin_timestamp_label = schema.Column(
+        types.DateTime(timezone=True), nullable=True)
+    inclusive_end_timestamp_label = schema.Column(
+        types.DateTime(timezone=True), nullable=True)
 
-    original_message = Column(Text, nullable=False)
-    content_block_count = Column(Integer)
+    original_message = schema.Column(types.Text, nullable=False)
+    content_block_count = schema.Column(types.Integer)
 
     # FIXME: should be a proper reference ID
-    destination_collections = Column(Text, nullable=True)
+    destination_collections = schema.Column(types.Text, nullable=True)
 
-    service_id = Column(
-        Text,
-        ForeignKey('services.id', onupdate="CASCADE", ondelete="CASCADE"))
+    service_id = schema.Column(
+        types.Text,
+        schema.ForeignKey(
+            'services.id', onupdate="CASCADE", ondelete="CASCADE"))
 
     service = relationship('Service', backref='inbox_messages')
 
@@ -170,39 +173,40 @@ class ResultSet(AbstractModel):
 
     __tablename__ = 'result_sets'
 
-    id = Column(String, primary_key=True)
+    id = schema.Column(types.String(150), primary_key=True)
 
-    collection_id = Column(
-        Integer,
-        ForeignKey(
+    collection_id = schema.Column(
+        types.Integer,
+        schema.ForeignKey(
             'data_collections.id', onupdate='CASCADE', ondelete='CASCADE'))
 
     collection = relationship('DataCollection', backref='result_sets')
 
-    bindings = Column(Text)
+    bindings = schema.Column(types.Text)
 
-    begin_time = Column(DateTime(timezone=True), nullable=True)
-    end_time = Column(DateTime(timezone=True), nullable=True)
+    begin_time = schema.Column(types.DateTime(timezone=True), nullable=True)
+    end_time = schema.Column(types.DateTime(timezone=True), nullable=True)
 
 
 class Subscription(AbstractModel):
 
     __tablename__ = 'subscriptions'
 
-    id = Column(String, primary_key=True)
+    id = schema.Column(types.String(150), primary_key=True)
 
-    collection_id = Column(
-        Integer,
-        ForeignKey(
+    collection_id = schema.Column(
+        types.Integer,
+        schema.ForeignKey(
             'data_collections.id', onupdate='CASCADE', ondelete='CASCADE'))
     collection = relationship('DataCollection', backref='subscriptions')
 
-    params = Column(Text, nullable=True)
+    params = schema.Column(types.Text, nullable=True)
 
     # FIXME: proper enum type
-    status = Column(String)
+    status = schema.Column(types.String(150))
 
-    service_id = Column(
-        String,
-        ForeignKey('services.id', onupdate="CASCADE", ondelete="CASCADE"))
+    service_id = schema.Column(
+        types.String(150),
+        schema.ForeignKey(
+            'services.id', onupdate="CASCADE", ondelete="CASCADE"))
     service = relationship('Service', backref='subscriptions')
