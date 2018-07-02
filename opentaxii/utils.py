b7af002b312ed1229493141aeb2605f092f8f82a
saaj <id@saaj.me>
2018-07-02 14:10:45 +0200

    Consistent JSON logging for Gunicorn via logging configuration that works with stdlib logging's and structlog's loggers.

--- opentaxii/utils.py
+++ opentaxii/utils.py
@@ -80,34 +80,46 @@ class PlainRenderer(object):
                 pairs=('{{{}}}'.format(pairs) if pairs else "")))
 
 
-def configure_logging(logging_levels, plain=False):
-
-    _remove_all_existing_log_handlers()
-
+def configure_logging(logging_levels, plain=False, stream=sys.stderr):
     renderer = (
         PlainRenderer() if plain else
         structlog.processors.JSONRenderer())
 
-    structlog.configure(
-        processors=[
-            structlog.stdlib.filter_by_level,
-            structlog.stdlib.add_logger_name,
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.PositionalArgumentsFormatter(),
-            structlog.processors.TimeStamper(fmt='iso'),
-            structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
-            renderer
-        ],
+    attr_processors = [
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.processors.TimeStamper(fmt='iso')
+    ]
+
+    structlog.configure_once(
+        processors=(
+            [structlog.stdlib.filter_by_level] +
+            attr_processors +
+            [
+                structlog.stdlib.PositionalArgumentsFormatter(),
+                structlog.processors.StackInfoRenderer(),
+                structlog.processors.format_exc_info,
+                structlog.stdlib.ProcessorFormatter.wrap_for_formatter
+            ]
+        ),
         context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
     )
 
-    handler = logging.StreamHandler(sys.stdout)
+    formatter = structlog.stdlib.ProcessorFormatter(
+        processor=renderer, foreign_pre_chain=attr_processors)
+
+    handler = AtomicStreamHandler(stream)
+    handler.setFormatter(formatter)
+
     root_logger = logging.getLogger()
-    root_logger.addHandler(handler)
+    root_logger.handlers = [handler]
+
+    # warnings issued by the ``warnings`` module will be
+    # redirected to the ``py.warnings`` logger
+    logging.captureWarnings(True)
 
     for logger, level in logging_levels.items():
 
@@ -117,13 +129,39 @@ def configure_logging(logging_levels, plain=False):
         logging.getLogger(logger).setLevel(level.upper())
 
 
-def _remove_all_existing_log_handlers():
-    for logger in logging.Logger.manager.loggerDict.values():
-        if hasattr(logger, 'handlers'):
-            del logger.handlers[:]
+class AtomicStreamHandler(logging.StreamHandler):
+    """A ``StreamHandler`` override that provides ``emit`` method which
+    does only one ``write`` per record to the underlying stream. This
+    is to keep an individual logging record per line. It's important
+    for JSON logging.
 
-    root_logger = logging.getLogger()
-    del root_logger.handlers[:]
+    In our case we have pre-forked gunicorn worker processes which
+    inherit ``stdout`` from the master process and write into it
+    concurrently [1]_. Kernel has the following guarantee for
+    write [2]_::
+
+        The adjustment of the file offset and the write operation
+        are performed as an atomic step.
+
+    On Python side, ``_io.TextIOWrapper``, which is the type of
+    ``sys.stdout``, doesn't provide explicit guarantee but experiments
+    show that for records of 8KB and 50 forked processes, lines are
+    not interleaved.
+
+    Output stream buffering [3]_ doesn't seem to have an effect.
+
+    .. [1]: https://github.com/benoitc/gunicorn/issues/1272
+    .. [2]: https://linux.die.net/man/2/write
+    .. [3]: https://eklitzke.org/stdout-buffering
+    """
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            self.stream.write('%s\n' % msg)
+            self.flush()
+        except Exception:
+            self.handleError(record)
 
 
 def sync_conf_dict_into_db(server, config, force_collection_deletion=False):
